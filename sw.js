0d42650875048b9ca53840e69445a5292d17b859
William <38344291+One-Old-Fella@users.noreply.github.com>
2025-12-20 23:59:56 -0800

    Update service worker cache version to v0.1.6.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'respire-system-v0.1.5.0';
+const CACHE_NAME = 'respire-system-v0.1.6.0';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -40,5 +40,6 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
